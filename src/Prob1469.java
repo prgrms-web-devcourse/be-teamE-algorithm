f36f8913e4e16984ef9992ef39e6c1a1f9be4352
HwiNyeon <z2b23173us@gmail.com>
2021-08-09 14:46:19 +0900

    (1469) 숌 사이 수열/DFS, Brute Force/오답
    * 정답 수열 찾는 과정에서 잘못된 칸에 접근하는 로직 수정
    * 이전 코드에 대한 오류 발생 케이스 예시
    3
    1 1 2
    
    * 조건 확인 추가 : 중복 숫자 발생시 2회 출현 조건 위반

--- src/Prob1469.java
+++ src/Prob1469.java
@@ -8,25 +8,52 @@ public class Prob1469 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        int[] nums = new int[N];
+        Integer[] nums = new Integer[N];
         answer = new int[N * 2];
+        // 중복숫자 검사.
+        Set<Integer> alreadyIn = new HashSet<>();
+        boolean impossible = false;
 
         for (int i = 0; i < N; i++) {
             nums[i] = sc.nextInt();
+            if (!alreadyIn.add(nums[i])) {
+                impossible = true;
+            }
         }
-        Arrays.sort(nums);
+
+        if (impossible) {
+            System.out.println(-1);
+            sc.close();
+            return;
+        }
+        // Arrays.sort(nums); // 사전순? 2가 10보다 앞에 오는 경우
+
+        // 사전순을 어떻게 정의?
+        // 10이 2보다 앞에 오는 경우
+        Comparator<Integer> comp = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                String n1 = String.valueOf(o1);
+                String n2 = String.valueOf(o2);
+                int i1 = n1.charAt(0) - '0';
+                int i2 = n2.charAt(0) - '0';
+                return i1 - i2;
+            }
+        };
+        Arrays.sort(nums, comp);
+
 
         // permutation 이용
-        permutation(nums, 0, N, N);
-        
+        permutation(nums, 0, N, N); // nPn
+
         if (!termi) {
             System.out.println(-1);
         }
-        
+
         sc.close();
     }
 
-    public static void permutation(int[] nums, int depth, int n, int r) {
+    public static void permutation(Integer[] nums, int depth, int n, int r) { // nPr
         if (termi) {
             return;
         }
@@ -48,32 +75,44 @@ public class Prob1469 {
         }
     }
 
-    public static void swap(int[] nums, int i, int j) {
+    public static void swap(Integer[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    public static boolean check(int[] nums) {
+    public static boolean check(Integer[] nums) {
         int[] currentAnswer = new int[2 * nums.length];
         Arrays.fill(currentAnswer, -1);
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i + nums[i] + 1 >= 2 * nums.length) {
+        for (int num : nums) {
+            int firstEmptySpace = getFirstEmptySpace(currentAnswer);
+
+            if (firstEmptySpace == -1 || firstEmptySpace + num + 1 >= 2 * nums.length) {
+                // 이미 꽉차있거나 (발생하면 안됨)
                 // 범위 초과
                 return false;
             }
 
-            if (currentAnswer[i] != -1 || currentAnswer[i + nums[i] + 1] != -1) {
+            if (currentAnswer[firstEmptySpace] != -1 || currentAnswer[firstEmptySpace + num + 1] != -1) {
                 // 이미 들어간 값이 있음
                 return false;
             }
 
-            currentAnswer[i] = nums[i];
-            currentAnswer[i + nums[i] + 1] = nums[i];
+            currentAnswer[firstEmptySpace] = num;
+            currentAnswer[firstEmptySpace + num + 1] = num;
         }
 
         answer = currentAnswer;
         return true;
+    }
+
+    public static int getFirstEmptySpace(int[] currentAnswer) {
+        for (int i = 0; i < currentAnswer.length; i++) {
+            if (currentAnswer[i] == -1) {
+                return i;
+            }
+        }
+        return - 1;
     }
 }
