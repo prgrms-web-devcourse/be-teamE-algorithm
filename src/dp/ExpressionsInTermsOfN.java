76ec8d98b6f46a27ac911b30fb21535e49193ffd
HwiNyeon <z2b23173us@gmail.com>
2021-08-23 15:29:11 +0900

    [Refactor] - 효율성개선 - (프로그래머스 42895) N으로 표현 / DP / 정답

--- src/dp/ExpressionsInTermsOfN.java
+++ src/dp/ExpressionsInTermsOfN.java
@@ -7,7 +7,6 @@ import java.util.Set;
 
 public class ExpressionsInTermsOfN {
     public int solution(int N, int number) {
-        int answer = -1;
         // 문제조건에 의해 8개 이상의 N은 사용할 수 없음
         List<Set<Integer>> list = new ArrayList<>();
         for (int i = 0 ; i < 8; i++) {
@@ -17,7 +16,7 @@ public class ExpressionsInTermsOfN {
 
         for (int i = 0; i < 8; i++) {
             // 1. N을 (i + 1)번 이어붙인 경우 where i = 0, 1, 2, ...
-            list.get(i).add(Integer.parseInt(repeat(N, i + 1)));
+            list.get(i).add(Integer.parseInt(repeat(N, i + 1))); //
 
             // 2. 이전 단계의 결과들를 서로 사칙연산한 결과
             // [N을 1개 사용한 경우] +-*/ [(i + 1) - 1개 사용한 경우]
@@ -26,23 +25,24 @@ public class ExpressionsInTermsOfN {
             for (int j = 0; j < i; j++) {
                 for (int case1 : list.get(j)) {
                     for (int case2 : list.get(i - 1 - j)) {
+                        // 정답 확인 로직
                         list.get(i).add(case1 + case2);
                         list.get(i).add(case1 - case2);
                         list.get(i).add(case1 * case2);
                         if (case2 != 0) {
                             list.get(i).add(case1 / case2);
                         }
+
                     }
                 }
             }
 
             if (list.get(i).contains(number)) {
-                answer = i + 1;
-                break;
+                return i + 1;
             }
         }
 
-        return answer;
+        return -1;
     }
 
     private String repeat(int N, int times) {
