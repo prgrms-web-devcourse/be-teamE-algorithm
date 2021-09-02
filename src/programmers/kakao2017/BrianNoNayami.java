d307d423dbfb35f00fe45fa5c835a898f780ac92
HwiNyeon <z2b23173us@gmail.com>
2021-09-02 15:25:24 +0900

    (1830) 브라언의 고민 - 나중에 다시풀기 위해 저장

--- src/programmers/kakao2017/BrianNoNayami.java
+++ src/programmers/kakao2017/BrianNoNayami.java
@@ -23,36 +23,21 @@ public class BrianNoNayami {
 				for (int i = 0; i < size; i++) {
 					String word = words.poll();
 
-					List<String> parsed = parseFirstWord(word);
-					if (parsed.size() == 2) {
-						// 이미 복원 완료된 단어가 있음 (index 0)
-						words.offer(parsed.remove(0));
-					}
-
 					// [규칙 2]의 효과 제거하기
-					parsed = parseByRuleTwo(parsed.get(0));
+					List<String> parsed = parseByRuleTwo(word);
 
 					// [규칙 2]의 중복 적용 여부 검증
-					for (String str : parsed) {
-						if (isLowerCase(str.charAt(0))) {
-							throw new RuntimeException("invalid");
-						}
+					String str = parsed.get(0);
+					if (isLowerCase(str.charAt(0))) {
+						throw new RuntimeException("invalid");
 					}
 
-					// [규칙 2]의 효과가 제거되었으므로 "bAAcAcAb" -> "AAcAcA" 가 되었으므로 맨 앞에 복원 완료된 단어가 나타날 수 있음
-					parsed = parseFirstWord(parsed);
-					// 맨 앞 단어 검증
-					String firstWord = parsed.get(0);
-					if (isOnlyUpperCase(firstWord)) {
-						words.offer(parsed.remove(0));
-					}
-
-					// parsed에 대해 [규칙 1]의 효과 제거하기 <--
+					// [규칙 1]의 효과 제거하기
 					parsed = parseByRuleOne(parsed);
 
-					for (String str : parsed) {
-						if (!str.isEmpty()) {
-							words.offer(str.trim());
+					for (String str2 : parsed) {
+						if (!str2.isEmpty()) {
+							words.offer(str2.trim());
 						}
 					}
 				}
@@ -69,74 +54,18 @@ public class BrianNoNayami {
 		return sb.toString().trim();
 	}
 
-	private List<String> parseFirstWord(List<String> parsed) {
-		List<String> result = parseFirstWord(parsed.remove(0));
-		result.addAll(parsed);
-		return result;
-
-	}
-
-	private List<String> parseFirstWord(String bigWord) {
-		// Length 2 -> [복원 완료된 단어, 복원 해야할 단어군]
-		// Length 1 -> [복원 해야 할 단어군]
-		List<String> result = new ArrayList<>();
-		if (isOnlyUpperCase(bigWord)) {
-			result.add(bigWord);
-			return result;
-		}
-
-		int length = bigWord.length();
-		int cutPoint = 0;
-		for (int i = 0; i < length; i++) {
-			if (isLowerCase(bigWord.charAt(i))) {
-				cutPoint = i;
-				break;
-			}
-		}
-
-		char firstOccurrenceChar = bigWord.charAt(cutPoint);
-		int count = countOccurrence(bigWord, firstOccurrenceChar); // 2이면 [규칙 2]로 간주하기
-		if (count == 2) {
-			try {
-				result.add(bigWord.substring(0, cutPoint));
-				result.add(bigWord.substring(cutPoint));
-			} catch (StringIndexOutOfBoundsException e) {
-				// 맨 앞에 복원 완료된 단어로 이루어진 부분이 없음
-				result.add(bigWord);
-			}
-		} else {
-			try {
-				result.add(bigWord.substring(0, cutPoint - 1));
-				result.add(bigWord.substring(cutPoint - 1));
-			} catch (StringIndexOutOfBoundsException e) {
-				// 맨 앞에 복원 완료된 단어로 이루어진 부분이 없음
-				result.add(bigWord);
-			}
-		}
-
-		while(result.remove("")) {}
-		return result;
-	}
-
 	private List<String> parseByRuleOne(List<String> parsed) {
-		// 맨 앞 단어에 대해서만 과정을 수행하고, 뒤에 남은 단어(들)은 이후 같은 과정을 반복하면서 알아서 처리되도록 한다.
-		List<String> result = new ArrayList<>(); // parsed.get(0)은 처리과정을 거친 후 result에 넣는다. 나머지 parsed의 원소들은 그냥 차례대로 result에 넣는다.
-		String word;
-		try {
-			word = parsed.remove(0);
-		} catch (IndexOutOfBoundsException e) {
-			// 더이상 남은 단어가 없음!
-			return result;
-		}
+		List<String> result = new ArrayList<>();
+		String word = parsed.remove(0);
 
-		if (word.length() == 1) {
-			// 길이 1인 완성된 단어이다. 볼 것도 없다.
+		if (isOnlyUpperCase(word)) {
 			result.add(word);
 			result.addAll(parsed);
 			return result;
 		}
 
-		int firstOccurrenceIndex = -1;
+		// 이하, word에는 반드시 1개 이상의 소문자가 있음
+		int firstOccurrenceIndex = 0;
 		int length = word.length();
 		for (int i = 1; i < length; i++) {
 			if (isLowerCase(word.charAt(i))) {
@@ -144,13 +73,24 @@ public class BrianNoNayami {
 				break;
 			}
 		}
+
 		char firstOccurrenceChar = word.charAt(firstOccurrenceIndex);
 		String[] split = word.split(String.valueOf(firstOccurrenceChar));
-		// [규칙 1]은 모든 글자 하나하나 마다 기호(소문자 영어)를 넣는다. 따라서 이 배열의 처음과 끝 원소를 제외한 모든 원소는 길이가 1이어야 한다.
-		// 그런데 이 배열의 길이가 2라면(= 즉 기호가 단 하나만 쓰였다면) 기호를 빼고 나뉘어진 두 구간을 합치면 원래 단어가 된다.
-		// 그 이상의 크기일경우, 맨 앞에 있는 원소의 마지막 글자 부터 맨 뒤에 있는 원소의 첫 번째 글자까지가 [규칙 1]에 의해 나뉜 원래 단어이다.
+		// [규칙 1]은 원래 단어의 모든 글자 하나하나 마다 기호(소문자 영어)를 넣는다.
 		if (split.length == 2) {
-			result.add(split[0] + split[1]);
+			String str1 = "";
+			String str2 = "";
+			String str3 = "";
+			try {
+				str1 = split[0].substring(0, split[0].length() - 1);
+				str2 = "" + split[0].charAt(split[0].length() - 1) + split[1].charAt(0);
+				str3 = split[1].substring(1);
+			} catch (IndexOutOfBoundsException e) {
+				// pass
+			}
+			result.add(str1);
+			result.add(str2);
+			result.add(str3);
 			result.addAll(parsed);
 			return result;
 		}
@@ -200,13 +140,13 @@ public class BrianNoNayami {
 
 	private List<String> parseByRuleTwo(String word) {
 		List<String> result = new ArrayList<>();
-		if (isUpperCase(word.charAt(0))) {
-			// [규칙 2]의 적용을 받지 않음. (내부에는 있을수도 있음)
+		if (isOnlyUpperCase(word)) {
+			// 이미 복원이 완료된 단어임
 			result.add(word);
 			return result;
 		}
-
-		int firstOccurrenceIndex = -1;
+		// 이하, 반드시 소문자가 한 개 이상 존재한다.
+		int firstOccurrenceIndex = 0;
 		int length = word.length();
 		for (int i = 0; i < length; i++) {
 			if (isLowerCase(word.charAt(i))) {
@@ -219,13 +159,13 @@ public class BrianNoNayami {
 		int count = countOccurrence(word, firstOccurrenceChar);
 		if (count != 2) {
 			// [규칙 2]의 적용을 받기 위해서는 반드시 소문자 2개가 있어야 함.
-			// 이는 [규칙 1]이 적용되는 것 임.
+			// [규칙 2]에 의한 파싱을 할 수 없다.
 			result.add(word);
 			return result;
 		}
 
 		int secondOccurrenceIndex = word.lastIndexOf(firstOccurrenceChar);
-		// [규칙 2]에 따르면 반드시 소문자 사이에 1개 이상의 대문자가 존재해야 함. -> Invalid
+		// [규칙 2]에 따르면 반드시 소문자 사이에 1개 이상의 대문자가 존재해야 한다.
 		if (secondOccurrenceIndex == firstOccurrenceIndex + 1) {
 			throw new RuntimeException("invalid");
 		}
